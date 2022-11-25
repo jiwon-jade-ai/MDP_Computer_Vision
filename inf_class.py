0dbb14f675b40d8c3f3dc482232995d1bb9e9c3a
Michael Ye <michye@umich.edu>
2022-11-24 20:50:49 -0500

    added multiple bounding boxes, but inefficient because multiple callbacks are created

--- inf_class.py
+++ inf_class.py
@@ -12,29 +12,26 @@ from cv_bridge import CvBridge, CvBridgeError
 bridge = CvBridge()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-percentage = 0
 MODEL_PATH = "Unet-diagdataset.pt"
 model = torch.load(MODEL_PATH)
 image_topic =  "/zed/zed_node/rgb/image_rect_color"
-road_percentage_pub = rospy.Publisher('cv_nav/road_percentage', Float64, queue_size=1)
-road_percentage_msg = Float64()
 
-def callback_image(msg):
-    global percentage
-    global road_percentage_pub
-    global road_percentage_msg
-    global model
-    print("Received an image!")
-    cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    image_resize = cv2.resize(cv2_img, (640, 384))
-    pred_mask = predict_image_mask_miou(model, image_resize)
-    cimage = pred_mask[248:328, 35:385] / 85 # sidewalk is 85
-    count = (cimage == 1.0).sum()
-    percentage = float(count / np.array(cimage).size * 100)
-    road_percentage_msg.data = percentage
-    road_percentage_pub.publish(road_percentage_msg)
-
+def create_callback(x_min, x_max, y_min, y_max, topic_name):
+    road_percentage_pub = rospy.Publisher(topic_name, Float64, queue_size=1)
+    road_percentage_msg = Float64()
+    def callback_image(msg):
+        global model
+        nonlocal road_percentage_pub
+        nonlocal road_percentage_msg
+        print("Received an image!")
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+        image_resize = cv2.resize(cv2_img, (640, 384))
+        pred_mask = predict_image_mask_miou(model, image_resize)
+        cimage = pred_mask[x_min:x_max, y_min:y_max] / 85 # sidewalk is 85
+        count = (cimage == 1.0).sum()
+        road_percentage_msg.data = float(count / np.array(cimage).size * 100)
+        road_percentage_pub.publish(road_percentage_msg)
+    return callback_image
 
 def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     model.eval()
@@ -53,8 +50,15 @@ def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229
 
 if __name__ == '__main__':
     rospy.init_node('segmenation_node')
-    #global image_topic
-    image_subscriber = rospy.Subscriber(image_topic, Image, callback_image)
+    box_callback_main = create_callback(248, 328, 35, 385, 'cv_nav/road_percentage/main')
+    box_callback_rotate = create_callback(200, 368, 300, 385, 'cv_nav/road_percentage/rotate')
+    box_callback_left = create_callback(0, 320, 100, 300, 'cv_nav/road_percentage/left')
+    box_callback_right = create_callback(320, 640, 100, 300, 'cv_nav/road_percentage/right')
+
+    main_subscriber = rospy.Subscriber(image_topic, Image, box_callback_main)
+    rotate_subscriber = rospy.Subscriber(image_topic, Image, box_callback_rotate)
+    left_subscriber = rospy.Subscriber(image_topic, Image, box_callback_left)
+    right_subscriber = rospy.Subscriber(image_topic, Image, box_callback_right)
     rospy.spin()
 
 
