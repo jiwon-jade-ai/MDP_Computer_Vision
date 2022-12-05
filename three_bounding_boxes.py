9c87e196e74fdd168e8d9063bf6bac79ee991acc
jiwon-jade-ai <justina3587@gmail.com>
2022-12-05 10:38:56 -0500

    Three bounding boxes
    Three bounding boxes: forward, rotateLeft, and rotateRight

--- three_bounding_boxes.py
+++ three_bounding_boxes.py
@@ -9,32 +9,39 @@ from sensor_msgs.msg import Image
 from torchvision import transforms as T
 from cv_bridge import CvBridge, CvBridgeError
 
+# Model
 bridge = CvBridge()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-percentage = 0
 MODEL_PATH = "Unet-diagdataset.pt"
 model = torch.load(MODEL_PATH)
+
+# Topic publishers
+forward_box_pub = rospy.Publisher('cv_nav/road_percentage/forward', Float64, queue_size=1)
+rotateLeft_box_pub = rospy.Publisher('cv_nav/road_percentage/rotateRight', Float64, queue_size=1)
+rotateRight_box_pub = rospy.Publisher('cv_nav/road_percentage/rotateLeft', Float64, queue_size=1)
 image_topic =  "/zed/zed_node/rgb/image_rect_color"
-road_percentage_pub = rospy.Publisher('cv_nav/road_percentage', Float64, queue_size=1)
 road_percentage_msg = Float64()
 
+
+# Bounding box metadata. Each bounding box is described by [publisher, x_min, x_max, y_min, y_max]
+forward_box_info = [forward_box_pub, 250, 321, 250, 391] # 250 391 250 320
+rotateLeft_box_info = [rotateLeft_box_pub, 270, 311, 320, 481]
+rotateRight_box_info = [rotateRight_box_pub, 270, 311, 160, 321]
+bounding_boxes = [forward_box_info, rotateLeft_box_info, rotateRight_box_info]
+
 def callback_image(msg):
-    global percentage
-    global road_percentage_pub
-    global road_percentage_msg
     global model
+    global road_percentage_msg
     print("Received an image!")
     cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
     image_resize = cv2.resize(cv2_img, (640, 384))
     pred_mask = predict_image_mask_miou(model, image_resize)
-    cimage = pred_mask[248:328, 35:385] / 85 # sidewalk is 85
-    count = (cimage == 1.0).sum()
-    percentage = float(count / np.array(cimage).size * 100)
-    road_percentage_msg.data = percentage
-    road_percentage_pub.publish(road_percentage_msg)
-
+    for bounding_box in bounding_boxes:
+        publisher, x_min, x_max, y_min, y_max = bounding_box
+        cimage = pred_mask[x_min:x_max, y_min:y_max] / 85 # sidewalk is 85
+        count = (cimage == 1.0).sum()
+        road_percentage_msg.data = float(count / np.array(cimage).size * 100)
+        publisher.publish(road_percentage_msg)
 
 def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     model.eval()
@@ -50,13 +57,7 @@ def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229
 
     return masked
 
-
 if __name__ == '__main__':
     rospy.init_node('segmenation_node')
-    #global image_topic
-    image_subscriber = rospy.Subscriber(image_topic, Image, callback_image)
+    sub = rospy.Subscriber(image_topic, Image, callback_image)
     rospy.spin()
-
-
-# load model
-# model.eval
